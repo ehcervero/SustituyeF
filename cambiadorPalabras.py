fadd918310c945660ca2571f08c0492f8ee63407
ehcervero <ehcervero@gmail.com>
2021-08-15 13:15:36 +0200

    He cambiado el programa para que en caso que no se escriban los tres parametros devuelva una respuesta para incluir los tres parametros

--- cambiadorPalabras.py
+++ cambiadorPalabras.py
@@ -1,24 +1,28 @@
 import sys
 
-argumentos = sys.argv
+if len(sys.argv) == 4:
 
-nombreFichero = argumentos[1]
-original = argumentos[2]
-nueva = argumentos[3]
+    nombreFichero = sys.argv[1]
+    original = sys.argv[2]
+    nueva = sys.argv[3]
 
-nombreFichero = "Fichero.txt"
+    nombreFichero = "Fichero.txt"
 
-f = open(nombreFichero,"r")
+    f = open(nombreFichero,"r")
 
-texto_original = f.read()
-f.close()
+    texto_original = f.read()
+    f.close()
 
-texto_sustituido = texto_original.replace(original, nueva)
+    texto_sustituido = texto_original.replace(original, nueva)
 
-f = open(nombreFichero,"w")
-f.write(texto_sustituido)
-f.close()
-
+    f = open(nombreFichero,"w")
+    f.write(texto_sustituido)
+    f.close()
+        
+else:
+  
+    print("El programa necesita 3 argumentos: Nombre Fichero, Original y Nueva")
+    sys.exit(1)
 
 
 
